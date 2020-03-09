1ea9fe42cd10d28f3404e13522da45bef29140fb
Abhishyant Khare <Abhishyant Khare>
2020-03-08 18:13:48 -0700

    RegisterShape from TF

--- astronet/contrib/framework/python/ops/gen_variable_ops.py
+++ astronet/contrib/framework/python/ops/gen_variable_ops.py
@@ -18,47 +18,52 @@ from tensorflow.python.framework import common_shapes as _common_shapes
 from tensorflow.python.framework import op_def_registry as _op_def_registry
 from tensorflow.python.framework import ops as _ops
 from tensorflow.python.framework import op_def_library as _op_def_library
+from astronet.contrib.framework.python.ops.old_ops import RegisterShape
 
 
 def zero_initializer(ref, name=None):
-  r"""Initialize 'ref' with all zeros. This op requires that the tensor is not
+    r"""Initialize 'ref' with all zeros. This op requires that the tensor is not
 
-  initialized. The tensor will first be allocated memory, then be filled with all
-  zeros. This op is intended to save memory during initialization,
-  if you use this op, you should not run initializer of the 'ref' tensor.
+    initialized. The tensor will first be allocated memory, then be filled with all
+    zeros. This op is intended to save memory during initialization,
+    if you use this op, you should not run initializer of the 'ref' tensor.
 
-  Args:
-    ref: A mutable `Tensor`. Must be one of the following types: `float32`, `float64`, `int32`, `int64`, `uint8`, `int16`, `int8`, `uint16`, `half`.
-      Should be from a `Variable` node.
-    name: A name for the operation (optional).
+    Args:
+      ref: A mutable `Tensor`. Must be one of the following types: `float32`, `float64`, `int32`, `int64`, `uint8`, `int16`, `int8`, `uint16`, `half`.
+        Should be from a `Variable` node.
+      name: A name for the operation (optional).
 
-  Returns:
-    Same as "ref".
-  """
-  _ctx = _context.context()
-  if _ctx.in_graph_mode():
-    _, _, _op = _op_def_lib._apply_op_helper(
-        "ZeroInitializer", ref=ref, name=name)
-    _result = _op.outputs[:]
-    _inputs_flat = _op.inputs
-    _attrs = ("T", _op.get_attr("T"))
-  else:
-    raise RuntimeError(
-        "zero_initializer op does not support eager execution. Arg 'output_ref'' is a ref.")
-  _execute.record_gradient(
-      "ZeroInitializer", _inputs_flat, _attrs, _result, name)
-  _result, = _result
-  return _result
+    Returns:
+      Same as "ref".
+    """
+    _ctx = _context.context()
+    if _ctx.in_graph_mode():
+        _, _, _op = _op_def_lib._apply_op_helper(
+            "ZeroInitializer", ref=ref, name=name)
+        _result = _op.outputs[:]
+        _inputs_flat = _op.inputs
+        _attrs = ("T", _op.get_attr("T"))
+    else:
+        raise RuntimeError(
+            "zero_initializer op does not support eager execution. Arg 'output_ref'' is a ref.")
+    _execute.record_gradient(
+        "ZeroInitializer", _inputs_flat, _attrs, _result, name)
+    _result, = _result
+    return _result
 
-_ops.RegisterShape("ZeroInitializer")(None)
+
+RegisterShape("ZeroInitializer")(None)
+
 
 def _InitOpDefLibrary(op_list_proto_bytes):
-  op_list = _op_def_pb2.OpList()
-  op_list.ParseFromString(op_list_proto_bytes)
-  _op_def_registry.register_op_list(op_list)
-  op_def_lib = _op_def_library.OpDefLibrary()
-  op_def_lib.add_op_list(op_list)
-  return op_def_lib
+    op_list = _op_def_pb2.OpList()
+    op_list.ParseFromString(op_list_proto_bytes)
+    _op_def_registry.register_op_list(op_list)
+    op_def_lib = _op_def_library.OpDefLibrary()
+    op_def_lib.add_op_list(op_list)
+    return op_def_lib
+
+
 # op {
 #   name: "ZeroInitializer"
 #   input_arg {
@@ -90,4 +95,5 @@ def _InitOpDefLibrary(op_list_proto_bytes):
 #   }
 #   allows_uninitialized_input: true
 # }
-_op_def_lib = _InitOpDefLibrary(b"\nO\n\017ZeroInitializer\022\013\n\003ref\"\001T\200\001\001\032\022\n\noutput_ref\"\001T\200\001\001\"\030\n\001T\022\004type:\r\n\0132\t\001\002\003\t\004\005\006\021\023\230\001\001")
+_op_def_lib = _InitOpDefLibrary(
+    b"\nO\n\017ZeroInitializer\022\013\n\003ref\"\001T\200\001\001\032\022\n\noutput_ref\"\001T\200\001\001\"\030\n\001T\022\004type:\r\n\0132\t\001\002\003\t\004\005\006\021\023\230\001\001")
